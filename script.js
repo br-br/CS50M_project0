7c63e2721e19a01af8bb215602907436392fc1a7
Britte Breindl <britte.breindl@gmail.com>
2019-06-03 16:03:12 +0200

    implemented itemcount and uncheckedcount; stub of createTodoItem adds empty li elements

--- script.js
+++ script.js
@@ -3,12 +3,48 @@ const classNames = {
   TODO_CHECKBOX: 'todo-checkbox',
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
+  TODO_TEXT: 'todo-text',
+  TODO_REF: 'todo-ref',
 }
 
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let itemCount = 0
+let uncheckedCount = 0
+let itemRef = 0
+
+function updateItemCount(difference) {
+  itemCount += difference
+  itemCountSpan.innerHTML = itemCount
+}
+
+function updateUncheckedCount(difference) {
+  uncheckedCount += difference
+  uncheckedCountSpan.innerHTML = uncheckedCount
+}
+
+function updateItemRef() {
+  itemRef += 1
+}
+
 function newTodo() {
-  alert('New TODO button clicked!')
+  let name = prompt("Give your task a name. (You may edit it later...)", "Do Something New...")
+
+  const todo = createTodoItem(name)
+  list.appendChild(todo)
+  updateItemCount(1)
+  updateUncheckedCount(1)
+  updateItemRef()
+}
+
+function createTodoItem(name) {
+
+  console.log(name)
+
+  const li = document.createElement('li')
+  li.className = classNames.TODO_ITEM
+
+  return li
 }
